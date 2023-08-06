b7e211d487761d8b66974f4fe000b106cbb67b7e
eggpoison <edwrd.magee@gmail.com>
2023-08-06 14:38:59 +1000

    Create slime puddle particles when flesh sword hits something

--- src/items/specific/FleshSword.ts
+++ src/items/specific/FleshSword.ts
@@ -1,9 +1,30 @@
+import { Vector, ParticleType, lerp } from "webgl-test-shared";
+import Particle from "../../Particle";
 import Entity from "../../entities/Entity";
 import WeaponItem from "../generic/WeaponItem";
 
 class FleshSword extends WeaponItem {
    public damageEntity(entity: Entity): void {
       entity.applyStatusEffect("poisoned", 3);
+
+      // Create slime puddle
+      const spawnPosition = entity.position.copy();
+      const offset = new Vector(30 * Math.random(), 2 * Math.PI * Math.random()).convertToPoint();
+      spawnPosition.add(offset);
+
+      const lifetime = 7.5;
+      
+      new Particle({
+         type: ParticleType.slimePuddle,
+         spawnPosition: spawnPosition,
+         initialVelocity: null,
+         initialAcceleration: null,
+         initialRotation: 2 * Math.PI * Math.random(),
+         opacity: (age: number): number => {
+            return lerp(0.75, 0, age / lifetime);
+         },
+         lifetime: lifetime
+      });
    }
 }
 
